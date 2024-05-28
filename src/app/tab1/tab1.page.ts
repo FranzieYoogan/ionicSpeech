e9b0b1cb18a406347cdff1da7ad746c9506396e4
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-27 22:01:24 -0300

    yaaay

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { HeaderPage } from '../header/header.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HeaderPage],
 })
 export class Tab1Page {
   constructor() {}
+
+  speech() {
+
+    const text:any = document.getElementById('text')
+
+    if(text.value) {
+
+
+ 
+
+    const synth = window.speechSynthesis;
+let speech = text.value
+const utterThis = new SpeechSynthesisUtterance(speech);
+ synth.speak(utterThis);
+
+text.style.border = "2px solid #5a557c"
+} else {
+
+
+  const synth = window.speechSynthesis;
+  let speech = "cualé, escreve aí, véi"
+  const utterThis = new SpeechSynthesisUtterance(speech);
+   synth.speak(utterThis);
+
+  text.style.border = "2px solid red"
+
+}
+
+  }
+
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, OnInit, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
@@ -10,10 +10,49 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
     addIcons({ triangle, ellipse, square });
   }
+
+  ngOnInit(): void {
+    const tab1:any = document.getElementById('tab1')
+    const tab2:any = document.getElementById('tab2')
+
+    if(window.location.href == "tabs/tab1") {
+
+      tab2.style.color = "black"
+      tab1.style.color = "#5a557c"
+    }
+
+    if(window.location.href == "tabs/tab2") {
+
+      tab2.style.color = "#5a557c"
+      tab1.style.color = "black"
+    }
+
+  }
+
+  
+
+  tab1() {
+    const tab1:any = document.getElementById('tab1')
+    const tab2:any = document.getElementById('tab2')
+
+    tab1.style.color = "#5a557c"
+    tab2.style.color = "black"
+
+  }
+
+  tab2() {
+    const tab1:any = document.getElementById('tab1')
+    const tab2:any = document.getElementById('tab2')
+
+    tab1.style.color = "black"
+    tab2.style.color = "#5a557c"
+
+  }
+
 }
